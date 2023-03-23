03d79150b22e6643fe1f674278baeaa2dd61b0e7
abel mesfin <abelmesfin555@gmail.com>
2023-03-23 23:55:49 +0300

    commited

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,18 @@
+#!/usr/bin/python3
 def pascal_triangle(n):
+    if n <= 0:
+        return []
+    
     triangle = [[1]]
+    
     for i in range(1, n):
         row = [1]
+        
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            prev_row = triangle[i - 1]
+            row.append(prev_row[j - 1] + prev_row[j])
+        
         row.append(1)
         triangle.append(row)
+    
     return triangle
