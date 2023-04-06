e5dfa03578bc97b0aac4636abe93d553bd9ecc0e
abel mesfin <abelmesfin555@gmail.com>
2023-04-06 23:22:09 +0300

    inital commit

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" min operaions """
+
+
+def minOperations(n):
+    """It takes an integer n as input and returns an integer that represents
+    the minimum number of operations required to reach n H characters
+    in the file."""
+    if n <= 1:
+        return 0
+    if n % 2 == 1:
+        return 0
+    else:
+        return minOperations(n // 2) + 2
