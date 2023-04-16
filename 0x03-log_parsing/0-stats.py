d7d0a824ea66d557769db1178cc2344e8b54cd8b
abel mesfin <abelmesfin555@gmail.com>
2023-04-16 22:04:30 +0300

    inital commit

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import sys
+
+def print_stats(total_size, status_codes):
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code]:
+            print("{}: {}".format(code, status_codes[code]))
+
+def main():
+    total_size = 0
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    count = 0
+    for line in sys.stdin:
+        count += 1
+        try:
+            parts = line.split()
+            size = int(parts[-1])
+            status = int(parts[-2])
+            total_size += size
+            status_codes[status] += 1
+        except (ValueError, IndexError):
+            continue
+        if count % 10 == 0:
+            print_stats(total_size, status_codes)
+        try:
+            sys.stdout.flush()
+        except IOError:
+            continue
+    print_stats(total_size, status_codes)
+
+if __name__ == '__main__':
+    main()
