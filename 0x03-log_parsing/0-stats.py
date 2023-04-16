ea446340bb9b9ecbb7558fad4bd73ad2d54ddc8c
abel mesfin <abelmesfin555@gmail.com>
2023-04-16 22:06:31 +0300

    requirement error fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
-
+"""..."""
 import sys
 
+
 def print_stats(total_size, status_codes):
+    """..."""
     print("File size: {}".format(total_size))
     for code in sorted(status_codes.keys()):
         if status_codes[code]:
             print("{}: {}".format(code, status_codes[code]))
 
+
 def main():
+    """..."""
     total_size = 0
     status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
     count = 0
@@ -29,6 +33,7 @@ def main():
         except IOError:
             continue
     print_stats(total_size, status_codes)
+
 
 if __name__ == '__main__':
     main()
