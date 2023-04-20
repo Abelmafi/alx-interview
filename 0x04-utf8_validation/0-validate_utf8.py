7a791f7b1b211dbfba826277ee8f7b7592954909
abel mesfin <abelmesfin555@gmail.com>
2023-04-21 01:10:12 +0300

    commited

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,37 +2,28 @@
 """0-validate_utf8.py"""
 
 def validUTF8(data):
-    n_bytes = 0
-    for i in range(8):
-        if (data[0] >> (7-i)) & 1:
-            n_bytes += 1
+    """Checks if a list of integers are valid UTF-8 codepoints.
+    See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
+    """
+    num_bytes = 0
+    n = len(data)
+    for i in range(n):
+        if data[i] > 255:
+            return False
+        if num_bytes == 0:
+            if (data[i] >> 7) == 0:
+                continue
+            elif (data[i] >> 5) == 0b110:
+                num_bytes = 2
+            elif (data[i] >> 4) == 0b1110:
+                num_bytes = 3
+            elif (data[i] >> 3) == 0b11110:
+                num_bytes = 4
+            else:
+                return False
         else:
-            break
+            if (data[i] >> 6) != 0b10:
+                return False
+            num_bytes -= 1
 
-    # Step 2: Check that the remaining bytes start with 10
-    if n_bytes == 1 or n_bytes > 4:
-        return False
-    for i in range(1, n_bytes):
-        if i >= len(data):
-            return False
-        if (data[i] >> 6) != 0b10:
-            return False
-
-    # Step 3: Check that the code point is valid
-    if n_bytes == 0:
-        code_point = data[0]
-    else:
-        mask = (1 << (n_bytes+1)) - 1
-        code_point = data[0] & mask
-        for i in range(1, n_bytes):
-            code_point = (code_point << 6) | (data[i] & 0b00111111)
-    if code_point < 0x80:
-        return False
-    elif code_point < 0x800:
-        return n_bytes == 2
-    elif code_point < 0x10000:
-        return n_bytes == 3
-    elif code_point < 0x110000:
-        return n_bytes == 4
-    else:
-        return False
+    return num_bytes == 0
