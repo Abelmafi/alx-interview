0557e7ec28086fd9f0f61d61d8159ff1631999fe
abel mesfin <abelmesfin555@gmail.com>
2023-04-21 00:35:53 +0300

    commited

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """0-validate_utf8.py"""
 
-
 def validUTF8(data):
     num_bytes = 0
 
@@ -10,9 +9,9 @@ def validUTF8(data):
             return False
 
         if num_bytes == 0:
-            if (i >> 7) == 0b0:
+            if (i >> 7) == 0:
                 num_bytes = 1
-            if (i >> 5) == 0b110:
+            elif (i >> 5) == 0b110:
                 num_bytes = 2
             elif (i >> 4) == 0b1110:
                 num_bytes = 3
@@ -21,9 +20,8 @@ def validUTF8(data):
             else:
                 return False
         else:
-            if ((i >> 6) != 0b10) and (num_bytes != 1):
+            if (i >> 6) != 0b10:
                 return False
             num_bytes -= 1
 
-
-    return True
+    return num_bytes == 0
