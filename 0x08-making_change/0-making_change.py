c9076d7b55cfc2785863a7306e725f2bdfd2ec6a
abel mesfin <abelmesfin555@gmail.com>
2023-05-21 20:34:48 +0300

    commited

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Task 1: '''
+
+
+def makeChange(coins, total):
+    '''...'''
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+    else:
+        return dp[total]
