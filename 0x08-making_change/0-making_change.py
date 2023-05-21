7a8f43e016cd611e63bb1030ba7f77bf11910d29
abel mesfin <abelmesfin555@gmail.com>
2023-05-21 20:52:32 +0300

    commited

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,15 +6,15 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    coins.sort(reverse=True)
+    num_coins = 0
 
     for coin in coins:
-        for i in range(coin, total + 1):
-            if dp[i - coin] != float('inf'):
-                dp[i] = min(dp[i], dp[i - coin] + 1)
+        num = total // coin
 
-    if dp[total] == float('inf'):
-        return -1
-    else:
-        return dp[total]
+        total -= num * coin
+        num_coins += num
+
+        if total == 0:
+            return num_coins
+    return -1
