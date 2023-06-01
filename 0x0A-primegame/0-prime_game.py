0208768cc359f1ebff941365c453dea30785517d
abel mesfin <abelmesfin555@gmail.com>
2023-06-02 00:02:04 +0300

    commited

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,73 +2,42 @@
 """defining is_winner function"""
 
 
-def isWinner(num_rounds, nums):
+def isWinner(x, nums):
     """
     Determine the winner of each game in the Prime Number Game.
+
     Args:
-        num_rounds (int): The number of rounds to play.
+        x (int): The number of rounds to play.
         nums (list): List of integers representing 'n' for each round.
 
     Returns:
         str or None: The name of the player with the most wins, or None if the winner cannot be determined.
     """
 
-    def sieve(n):
+    def is_prime(n):
         """
-        Apply the Sieve of Eratosthenes algorithm to generate prime numbers up to 'n'.
+        Check if a number is prime.
 
         Args:
-            n (int): The number to generate prime numbers up to.
+            n (int): The number to check.
 
         Returns:
-            list: A boolean list representing prime numbers up to 'n'.
+            bool: True if the number is prime, False otherwise.
         """
-        primes = [True] * (n + 1)
-        primes[0] = primes[1] = False
-        p = 2
-        while p * p <= n:
-            if primes[p]:
-                for i in range(p * p, n + 1, p):
-                    primes[i] = False
-            p += 1
-        return primes
-
-    def play_game(n):
-        """
-        Simulate the game for a given number 'n' and determine the winner.
-
-        Args:
-            n (int): The number for the current game.
-
-        Returns:
-            str: The name of the winner ('Maria' or 'Ben').
-        """
-        primes = sieve(n)
-        if primes[n]:
-            return "Maria"  # Maria wins if 'n' is prime
-
-        largest_prime = 0
-        for i in range(n, 1, -1):
-            if primes[i]:
-                largest_prime = i
-                break
-
-        if largest_prime <= 2:
-            return "Ben"  # Ben wins if largest prime <= 2
-
-        for i in range(2, largest_prime):
-            if primes[i] and play_game(n - i) == "Ben":
-                return "Maria"
-
-        return "Ben"
+        if n < 2:
+            return False
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
 
     maria_wins = 0
     ben_wins = 0
+
     for n in nums:
-        winner = play_game(n)
-        if winner == "Maria":
+        if is_prime(n):
             maria_wins += 1
-        elif winner == "Ben":
+        else:
             ben_wins += 1
 
     if maria_wins > ben_wins:
